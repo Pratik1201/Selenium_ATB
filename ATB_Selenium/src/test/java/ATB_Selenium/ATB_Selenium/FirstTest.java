5292296288931bb4c669b46f1e10ab4d10f8c98d
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-18 10:23:09 +0530

    Create new Maven project and added dependencies

---
+++ ATB_Selenium/src/test/java/ATB_Selenium/ATB_Selenium/FirstTest.java
@@ -0,0 +1,13 @@
+package ATB_Selenium.ATB_Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://automationexercise.com");
+	}
+
+}
