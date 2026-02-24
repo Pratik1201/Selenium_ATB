db7a8f3ce1c209c60ac1979c35d5b4619d4855ee
Pratik1201 <pratikhsonawane@gmail.com>
2026-02-24 22:45:04 +0530

    Added TC case with validation

--- ATB_Selenium/src/test/java/ATB_Selenium/ATB_Selenium/SecondTest.java
+++ ATB_Selenium/src/test/java/ATB_Selenium/ATB_Selenium/SecondTest.java
@@ -1,7 +1,10 @@
 package ATB_Selenium.ATB_Selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SecondTest {
@@ -11,6 +14,20 @@ public class SecondTest {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://practicetestautomation.com/practice-test-login");
 		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("student");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password123");
+		driver.findElement(By.id("submit")).click();
+		System.out.println(driver.findElement(By.xpath("//p[@class='has-text-align-center']")).getText());
+		String currentURL = driver.getCurrentUrl();
+		Assert.assertTrue(currentURL.contains("practicetestautomation.com/logged-in-successfully/"),"URL does not contain expected path. Actual URL:" + currentURL);
+		WebElement text = driver.findElement(By.xpath("//h1[@class='post-title']"));
+		String textMessage = text.getText();
+		Assert.assertTrue(textMessage.contains("Logged In Successfully") , "Actual Text:" + textMessage);
+		WebElement logoutBtn = driver.findElement(By.xpath("//a[contains(text(),'Log out')]"));
+		Assert.assertTrue(logoutBtn.isDisplayed());
+		driver.quit();
+		
+		
 		
 		
 	}
