b183984d3bb83436c328eed46f780d8ababef095
Pratik1201 <pratikhsonawane@gmail.com>
2026-03-10 22:00:56 +0530

    modified code

--- ATB_Selenium/src/test/java/ATB_Selenium/ATB_Selenium/AuutomationPractice.java
+++ ATB_Selenium/src/test/java/ATB_Selenium/ATB_Selenium/AuutomationPractice.java
@@ -1,5 +1,6 @@
 package ATB_Selenium.ATB_Selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,11 @@ public class AuutomationPractice {
 		driver.manage().window().maximize();
 		driver.get("https://awesomeqa.com/practice.html");
 		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Sumit");
+		driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Patel");
+		driver.findElement(By.id("sex-0")).click();
+		driver.findElement(By.id("exp-2")).click();
+		driver.findElement(By.id("datepicker")).sendKeys("12/01/1994");
 	}
 
 }
